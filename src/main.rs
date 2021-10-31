d04f45a04beb512637afbb9efb0f3ced1da249ac
hotgluebanjo <66244111+hotgluebanjo@users.noreply.github.com>
2021-10-31 21:50:08 +0000

    Use ratios instead of literal coordinates
    Now works with different resolutions and aspect ratios.

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,25 @@
-// NOTE: The Y axis is inverted in the image crate. At least
+// NOTE: The Y axis is inverted in the image crate. Or at least
 // it's the opposite of Nuke. In Nuke, the bottom of the image
 // is 0 and the top is 1079. Inverse of that here.
 // Took forever to figure out.
 //
 // Usage:
-// - Make sure image is 1920x1080
-// - Scale up and stretch until the patches hit the frame edges
+// - Scale up and stretch the image until edges of the color patches
+//   hit the edges of the frame
 // - Export a JPG or PNG
 use image::{ImageBuffer, Rgb};
 use std::{fmt, fs::OpenOptions, io::Write, process};
 use structopt::StructOpt;
 
+type Image = ImageBuffer<Rgb<f32>, Vec<f32>>;
+type Triplet = [f32; 3];
+type CheckerPoints<'a> = &'a [Point2d<f32>];
+
 const USAGE_INSTRUCTIONS: &str = "Extract datasets from a ColorChecker.
 Notes:
 This is a very simple app, so it's a little finicky. Here's how to prep images for it:
-- Make sure the image provided is 1920x1080
-- Scale up and stretch until the patches hit the frame edges
+- Scale up and stretch the image until edges of the color patches hit the edges of the frame
 - Export a JPG or PNG";
-
-// Totally unscientific
-const COLORCHECKER_CLASSIC_COORDS: [Point2d; 24] = [
-    Point2d { x: 145, y: 129 },
-    Point2d { x: 480, y: 129 },
-    Point2d { x: 810, y: 129 },
-    Point2d { x: 1135, y: 129 },
-    Point2d { x: 1460, y: 129 },
-    Point2d { x: 1790, y: 129 },
-    Point2d { x: 145, y: 405 },
-    Point2d { x: 480, y: 405 },
-    Point2d { x: 810, y: 405 },
-    Point2d { x: 1135, y: 405 },
-    Point2d { x: 1460, y: 405 },
-    Point2d { x: 1790, y: 405 },
-    Point2d { x: 145, y: 679 },
-    Point2d { x: 480, y: 679 },
-    Point2d { x: 810, y: 679 },
-    Point2d { x: 1135, y: 679 },
-    Point2d { x: 1460, y: 679 },
-    Point2d { x: 1790, y: 679 },
-    Point2d { x: 145, y: 959 },
-    Point2d { x: 480, y: 959 },
-    Point2d { x: 810, y: 959 },
-    Point2d { x: 1135, y: 959 },
-    Point2d { x: 1460, y: 959 },
-    Point2d { x: 1790, y: 959 },
-];
 
 macro_rules! attempt {
     ($x:expr, $message:expr) => {
@@ -61,38 +36,115 @@ macro_rules! attempt {
     };
 }
 
-#[derive(Debug)]
-struct Point2d {
-    x: u32,
-    y: u32,
+// Relative to 1.0 instead of 1080 or whatever specific resolution.
+const COLORCHECKER_CLASSIC: CheckerPoints = &[
+    Point2d {
+        x: 0.0755,
+        y: 0.1194,
+    },
+    Point2d {
+        x: 0.2500,
+        y: 0.1194,
+    },
+    Point2d {
+        x: 0.4218,
+        y: 0.1194,
+    },
+    Point2d {
+        x: 0.5911,
+        y: 0.1194,
+    },
+    Point2d {
+        x: 0.7604,
+        y: 0.1194,
+    },
+    Point2d {
+        x: 0.9322,
+        y: 0.1194,
+    },
+    Point2d {
+        x: 0.0755,
+        y: 0.3750,
+    },
+    Point2d {
+        x: 0.2500,
+        y: 0.3750,
+    },
+    Point2d {
+        x: 0.4218,
+        y: 0.3750,
+    },
+    Point2d {
+        x: 0.5911,
+        y: 0.3750,
+    },
+    Point2d {
+        x: 0.7604,
+        y: 0.3750,
+    },
+    Point2d {
+        x: 0.9322,
+        y: 0.3750,
+    },
+    Point2d {
+        x: 0.0755,
+        y: 0.6287,
+    },
+    Point2d {
+        x: 0.2500,
+        y: 0.6287,
+    },
+    Point2d {
+        x: 0.4218,
+        y: 0.6287,
+    },
+    Point2d {
+        x: 0.5911,
+        y: 0.6287,
+    },
+    Point2d {
+        x: 0.7604,
+        y: 0.6287,
+    },
+    Point2d {
+        x: 0.9322,
+        y: 0.6287,
+    },
+    Point2d {
+        x: 0.0755,
+        y: 0.8879,
+    },
+    Point2d {
+        x: 0.2500,
+        y: 0.8879,
+    },
+    Point2d {
+        x: 0.4218,
+        y: 0.8879,
+    },
+    Point2d {
+        x: 0.5911,
+        y: 0.8879,
+    },
+    Point2d {
+        x: 0.7604,
+        y: 0.8879,
+    },
+    Point2d {
+        x: 0.9322,
+        y: 0.8879,
+    },
+];
+
+/// Supported ColorCheckers
+enum Colorchecker<'a> {
+    /// Can be used for other similar ColorCheckers with a close enough pattern
+    Classic(CheckerPoints<'a>),
 }
 
-#[derive(Debug)]
-struct Dataset {
-    x: Vec<[f32; 3]>,
-}
-
-impl Dataset {
-    fn new(x: Vec<[f32; 3]>) -> Self {
-        Self { x }
-    }
-
-    fn from_colorchecker(image: ImageBuffer<Rgb<f32>, Vec<f32>>, coords: [Point2d; 24]) -> Self {
-        let mut values = Vec::new();
-        for i in coords.iter() {
-            // values.push(image.get_pixel(i.x, i.y).0);
-            values.push(Self::average_patch(image.clone(), i.x, i.y, 85));
-        }
-        Self::new(values)
-    }
-
+impl Colorchecker<'_> {
     // Very bad style
-    fn average_patch(
-        image: ImageBuffer<Rgb<f32>, Vec<f32>>,
-        x: u32,
-        y: u32,
-        radius: u32,
-    ) -> [f32; 3] {
+    fn average_patch(image: Image, x: u32, y: u32, radius: u32) -> Triplet {
         let (mut red, mut green, mut blue, mut num) = (0.0, 0.0, 0.0, 0.0);
         let (width, height) = image.dimensions();
 
@@ -114,11 +166,43 @@ impl Dataset {
     }
 }
 
+#[derive(Debug)]
+struct Point2d<T> {
+    x: T,
+    y: T,
+}
+
+#[derive(Debug)]
+struct Dataset(Vec<Triplet>);
+
+impl Dataset {
+    fn from_colorchecker(image: Image, colorchecker: Colorchecker) -> Self {
+        let patch_positions = match colorchecker {
+            Colorchecker::Classic(p) => p,
+        };
+
+        let (width, height) = image.dimensions();
+
+        let values = patch_positions
+            .iter()
+            .map(|p| {
+                let calc_width = (width as f32 * p.x) as u32;
+                let calc_height = (height as f32 * p.y) as u32;
+
+                Colorchecker::average_patch(image.clone(), calc_width, calc_height, 80)
+            })
+            .collect::<Vec<Triplet>>();
+
+        Self(values)
+    }
+}
+
 impl fmt::Display for Dataset {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        for i in &self.x {
+        for i in &self.0 {
             writeln!(f, "{:.20} {:.20} {:.20}", i[0], i[1], i[2])?;
         }
+
         Ok(())
     }
 }
@@ -128,7 +212,6 @@ impl fmt::Display for Dataset {
 #[structopt(
     name = "Get ColorChecker Values",
     about = USAGE_INSTRUCTIONS
-
 )]
 struct Cli {
     /// Image of color checker
@@ -144,7 +227,9 @@ fn main() {
 
     println!("Reading image..."); // Takes long enough
     let image = attempt!(image::open(args.filename), "Can't open image").to_rgb32f();
-    let dataset = Dataset::from_colorchecker(image, COLORCHECKER_CLASSIC_COORDS);
+    let colorchecker = Colorchecker::Classic(COLORCHECKER_CLASSIC);
+
+    let dataset = Dataset::from_colorchecker(image, colorchecker);
 
     if let Some(name) = args.output_name {
         let mut file = OpenOptions::new()
